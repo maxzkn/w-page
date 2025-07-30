7b142c4d63e6be1eadb7e939439b50920df566ad
maksim-zykin-tg <maksim.zykin@transfergo.com>
2025-07-30 02:00:53 +0100

    fixes

--- src/app/[locale]/components/hero-navigation.tsx
+++ src/app/[locale]/components/hero-navigation.tsx
@@ -39,8 +39,8 @@ export function LanguageSwitcher({
 
 export const HeroNavigation = ({ locale }: { locale: string }) => {
   return (
-    <div className="absolute top-4 sm:top-8 right-4 sm:right-10 z-20 flex items-center gap-3 safe-top">
-      <ViewGalleryButton className="inline-block px-1 py-1 text-main text-white border-b border-transparent hover:border-white transition uppercase" />
+    <div className="absolute top-4 sm:top-8 right-4 sm:right-10 z-20 flex items-center gap-5 safe-top px-5">
+      <ViewGalleryButton className="inline-block px-1 py-1 text-sm text-white border-b border-transparent hover:border-white transition uppercase" />
       <LanguageSwitcher currentLocale={locale} />
     </div>
   );
